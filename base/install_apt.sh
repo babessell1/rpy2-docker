e3b51d10c1085970398efd1f74308007db6d2169
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-02-27 18:23:51 -0500

    Update install_apt.sh

--- base/install_apt.sh
+++ base/install_apt.sh
@@ -14,9 +14,11 @@ apt-get install -y \
 
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 
-add-apt-repository \
-        --yes \
-		"deb {CRAN_MIRROR}/bin/linux/ubuntu/ $(lsb_release -cs)-cran40/"
+add-apt-repository -r ppa:marutter/rrutter4.0
+add-apt-repository -r "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/"
+#add-apt-repository \
+#        --yes \
+#		"deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/"
         #"deb {CRAN_MIRROR}/bin/linux/ubuntu/ $(lsb_release -c -s)${CRAN_MIRROR_TAG}/"
 
 apt update
