2a3698c72dd8b19fc968c023c8face9c0559008f
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-02-27 19:13:18 -0500

    Update install_apt.sh

--- base/install_apt.sh
+++ base/install_apt.sh
@@ -2,7 +2,6 @@ apt-get update -qq
 apt-get install -y \
         apt-utils \
         apt-transport-https \
-		dirmngr \
         gnupg \
 		libcurl4-openssl-dev \
 		libnlopt-dev \
@@ -11,20 +10,14 @@ apt-get install -y \
 #        --keyserver keyserver.ubuntu.com \
 #        --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 		
-
+apt install --no-install-recommends software-properties-common dirmngr
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
-#add-apt-repository -r ppa:marutter/rrutter4.0
-#add-apt-repository -r "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/"
-
-add-apt-repository \
-        --yes \
-		ppa:marutter/rrutter4.0
-
-
-add-apt-repository \
-        --yes \
-		"deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/"
+#add-apt-repository \
+#        --yes \
+#
+#		"deb {CRAN_MIRROR}/bin/linux/ubuntu/ $(lsb_release -c -s)-cran40/"
         #"deb {CRAN_MIRROR}/bin/linux/ubuntu/ $(lsb_release -c -s)${CRAN_MIRROR_TAG}/"
 
 apt update
