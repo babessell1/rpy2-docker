8271ad8302c07962315a6bf4c280396b15b93419
Brandt Bessell <60202219+babessell1@users.noreply.github.com>
2022-02-27 19:05:02 -0500

    Update install_apt.sh

--- base/install_apt.sh
+++ base/install_apt.sh
@@ -14,11 +14,17 @@ apt-get install -y \
 
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 
-add-apt-repository -r ppa:marutter/rrutter4.0
-add-apt-repository -r "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/"
-#add-apt-repository \
-#        --yes \
-#		"deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/"
+#add-apt-repository -r ppa:marutter/rrutter4.0
+#add-apt-repository -r "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/"
+
+add-apt-repository \
+        --yes \
+		ppa:marutter/rrutter4.0
+
+
+add-apt-repository \
+        --yes \
+		"deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/"
         #"deb {CRAN_MIRROR}/bin/linux/ubuntu/ $(lsb_release -c -s)${CRAN_MIRROR_TAG}/"
 
 apt update
@@ -29,15 +35,15 @@ apt-get install -y \
         aptdaemon \
         ed \
         git \
-	mercurial \
-	libcairo-dev \
-	libedit-dev \
-	libxml2-dev \
-	python3 \
-	python3-pip \
-	python3-venv \
-	r-base \
-	r-base-dev\
-	sudo \
-	wget
+		mercurial \
+		libcairo-dev \
+		libedit-dev \
+		libxml2-dev \
+		python3 \
+		python3-pip \
+		python3-venv \
+		r-base \
+		r-base-dev\
+		sudo \
+		wget
 rm -rf /var/lib/apt/lists/*
